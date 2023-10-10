5327ee52bb9d6786148068b569cf354a021ecf56
dinesh2016b <dinesh2016b@gmail.com>
2023-10-09 20:50:10 -0400

    Initial Commit

---
+++ eureka-server/src/main/java/com/online/eurekaserver/EurekaServerMainApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.online.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ */
+@EnableEurekaServer
+@SpringBootApplication(scanBasePackages = "com.online")
+public class EurekaServerMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerMainApplication.class, args);
+	}
+}
